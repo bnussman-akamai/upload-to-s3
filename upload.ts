d84b26588f425d5e5f913484f5f3d357a1e432f2
Banks Nussman <banks@nussman.us>
2025-08-11 22:26:11 -0400

    only upload last item

--- upload.ts
+++ upload.ts
@@ -2,7 +2,13 @@ import { Glob, s3 } from "bun";
 
 const glob = new Glob("*.tar.gz");
 
-for (const file of glob.scanSync(".")) {
-    await s3.write(file, Bun.file(file))
-    console.log("Uploaded", file, "to S3 bucket.")
+const file = Array.from(glob.scanSync(".")).toSorted().at(-1);
+
+if (!file) {
+  throw new Error("No build found to upload to S3 buckte");
 }
+
+await s3.write(file, Bun.file(file))
+
+console.log("Uploaded", file, "to S3 bucket.")
+
